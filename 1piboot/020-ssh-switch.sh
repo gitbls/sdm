505d09678c3e993042a4a397c97d6e90b3a59017
bls <bls3427@outlook.com>
2020-09-19 08:23:09 -0700

    Start ssh.socket on first boot

--- 1piboot/020-ssh-switch.sh
+++ 1piboot/020-ssh-switch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 logger "1piboot Setting up socket-managed SSH..."
-systemctl disable ssh.service
-systemctl enable ssh.socket
+systemctl disable --now ssh.service
+
+# Uncomment this line V to enable root to login with a password. If not changed, only ssh key can be used for root login
 #sed -i "s/\#PermitRootLogin prohibit-password/\#PermitRootLogin prohibit-password\nPermitRootLogin Yes\n/" /etc/ssh/sshd_config
-# Need this if IPV6 is disabled on Pi
+
+# Uncomment this line V if IPV6 is disabled on Pi
 #echo "AddressFamily inet" >> /etc/ssh/sshd_config
+
+systemctl enable ssh.socket
+systemctl start ssh.socket
