4a8d98b3dcd2a1db97aa8822d1369b414e24b7fb
bls <bls3427@outlook.com>
2021-08-31 16:08:53 -0700

    Use journal.conf.d for journal config changes

--- 1piboot/030-disable-rsyslog.sh
+++ 1piboot/030-disable-rsyslog.sh
@@ -2,11 +2,19 @@
 
 logger "FirstBoot: Disabling rsyslog and creating permanent journal..."
 systemctl disable rsyslog.service
-sed -i "s/\#ForwardToSyslog=yes/ForwardToSyslog=no/" /etc/systemd/journald.conf
+mkdir -p /etc/systemd/journald.conf.d
+cat > /etc/systemd/journald.conf.d/030-sdm-no-syslog.conf <<EOF
+[Journal]
+ForwardToSyslog=no
+EOF
 [ ! -d /var/log/journal ] && mkdir /var/log/journal
 systemd-tmpfiles --create --prefix /var/log/journal
 #
 # Turn off logrotate for rsyslog
 #
-[ ! -d /etc/logrotate.d/.save ] && mkdir /etc/logrotate.d/.save
-[ -f /etc/logrotate.d/rsyslog ] && mv /etc/logrotate.d/rsyslog /etc/logrotate.d/.save/rsyslog
+if [ -f /etc/logrotate.d/rsyslog ]
+then
+   mkdir -p /etc/logrotate.d/.sdmsave
+   mv /etc/logrotate.d/rsyslog /etc/logrotate.d/.sdmsave/rsyslog
+fi
+   
